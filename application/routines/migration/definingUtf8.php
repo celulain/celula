7f81b1cde90e7de5eb2f219569ccda9c9b978f8e
andrehigher <andrelopesgonzaga@gmail.com>
2012-12-24 11:18:04 -0200

    Routine to change names to utf8

--- application/routines/migration/definingUtf8.php
+++ application/routines/migration/definingUtf8.php
@@ -6,7 +6,8 @@ mysql_connect("localhost","root","[=celula]mysql");
 mysql_select_db($db);
 
 $sql = "
-	SELECT 
+	SELECT
+		user_id, 
 		name, 
 		surname,
 		nickname
@@ -15,6 +16,42 @@ $sql = "
 ";
 $rs = mysql_query($sql);
 while($col = mysql_fetch_array($rs)){
-	echo $col['name']."\n";
+	$sql_update = "
+		UPDATE 
+			core_user
+		SET 
+			name='".utf8_encode($col['name'])."',
+			surname='".utf8_encode($col['surname'])."',
+			nickname='".utf8_encode($col['nickname'])."'
+		WHERE 
+			user_id=".$col['user_id']."
+	";
+
+	echo $sql_update."\n";
+}
+
+
+$sql_address = "
+	SELECT 
+		user_id,
+		address,
+		district
+	FROM 
+		core_user_address
+";
+
+$rs_address = mysql_query($sql_address);
+while($col = mysql_fetch_array($rs_address)){
+	$sql_update = "
+		UPDATE 
+			core_user_address
+		SET 
+			address='".utf8_encode($col['address'])."',
+			district='".utf8_encode($col['district'])."'
+		WHERE 
+			user_id=".$col['user_id']."
+	";
+
+	echo $sql_update."\n";
 }
 ?>
